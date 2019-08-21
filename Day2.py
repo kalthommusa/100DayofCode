33a6ae2fa7eda5644f6db2f90f27ebe4ce0c7c97
Kalthom Musa <kalthommusa12345@gmail.com>
2019-08-21 18:36:16 +0300

    Day2

---
+++ Day2.py
@@ -0,0 +1,11 @@
+
+#This is a single line comment
+print("It's about comments") #this is a command to display a string
+
+
+"""
+If the condition is true, 
+the string statement will be displayed
+"""
+if (5 > 1):
+  print("Successfully passed Day2")
