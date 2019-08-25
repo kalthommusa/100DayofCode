962360508e80e41cf12574ab3061eaf6fc2bf483
Kalthom Musa <kalthommusa12345@gmail.com>
2019-08-25 21:45:07 +0300

    Day6

---
+++ Day6.py
@@ -0,0 +1,29 @@
+#x wil be 6
+x=int(6)
+#x wil be 6
+y=int(6.8)
+#x wil be 6
+z=int("6")
+print(x)
+print(y)
+print(z)
+
+#x wil be 6.0
+x=float(6)
+#x wil be 6.8
+y=float(6.8)
+#x wil be 6.0
+z=float("6")
+print(x)
+print(y)
+print(z)
+
+#x wil be "Day6"
+x=str("Day6")
+#x wil be "6"
+y=str(6)
+#x wil be "6.8"
+z=str("6.8")
+print(x)
+print(y)
+print(z)
