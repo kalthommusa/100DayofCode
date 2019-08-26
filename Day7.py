6e820435574eb59aa338403f1c2342f5e85ccd89
Kalthom Musa <kalthommusa12345@gmail.com>
2019-08-26 21:11:59 +0300

    Day7

---
+++ Day7.py
@@ -0,0 +1,12 @@
+#three quotes for a multiline string
+x="""Successfully 
+passed 
+Day7."""
+#return a string
+print(x)
+
+#return a character in index/position 0
+print(x[0])
+
+# return a substring word
+print(x[0:7])
