91709b1d6d190ccb4583d743f982fe05846f9b26
Kalthom Musa <kalthommusa12345@gmail.com>
2019-08-28 00:19:01 +0300

    Day8

---
+++ Day8.py
@@ -0,0 +1,15 @@
+x= " Successfully passed Day7 "
+"""returns the string without whitespace
+from the beginning or the end
+"""
+print(x.strip())
+#returns the length of the string
+print(len(x))
+#returns the string in lower case
+print(x.lower())
+#returns the string in upper case
+print(x.upper())
+#replaces a string with another string
+print(x.replace("passed","completed"))
+#splits the string into substrings
+print(x.split())
