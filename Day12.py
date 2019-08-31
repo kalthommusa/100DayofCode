5751e09b905cc92dffe1a69b299605c1fd397613
Kalthom Musa <kalthommusa12345@gmail.com>
2019-08-31 07:43:11 +0300

    Day12

---
+++ Day12.py
@@ -0,0 +1,13 @@
+x=5
+y=7
+
+Day12="Please, I want {} sandwishes and {} donutes"
+
+Day12=Day12.replace("I","we")
+print(Day12)
+
+Day12=Day12.format(x,y)
+print(Day12)
+
+Day12=Day12.replace("a","A")
+print(Day12)
