b40d0f2536ee2cb01399ac4ed1ad12edcec57ee4
Kalthom Musa <kalthommusa12345@gmail.com>
2019-09-07 01:03:50 +0300

    Day19

--- Day19.py
+++ Day19.py
@@ -1,4 +1,4 @@
 #task 2 for week3
-x=("Java" , "Paython" , "Swift")
-if "Paython" in x:
+x=("Java" , "Python" , "Swift")
+if "Python" in x:
 	print("Congrats! You just passed Day19 of the 100DayOfCode challenge")
