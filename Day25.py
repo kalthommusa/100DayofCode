633105c6de04ce57974b03be2207a1a074c7a056
Kalthom Musa <kalthommusa12345@gmail.com>
2019-09-14 17:34:33 +0300

    Day25

---
+++ Day25.py
@@ -0,0 +1,12 @@
+#task1 for week4
+set={1,3,5,7,8}
+print(set)
+#add the following numbers 4,8,12 to the set
+set.update([4,8,12])
+print(set)
+#remove number 8
+set.remove(8)
+print(set)
+#empty the whole set
+set.clear()
+print(set)
