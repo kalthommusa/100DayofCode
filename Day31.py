4ae7004469085770f719f170a5770cb9a077125f
Kalthom Musa <kalthommusa12345@gmail.com>
2019-09-19 15:31:47 +0300

    Day31

---
+++ Day31.py
@@ -0,0 +1,11 @@
+"""
+create a function with one parameter
+to print the full name when it is called 
+"""
+def my_function(fname):
+	print(fname+" "+"Musa")
+
+#call a function and pass a first name
+my_function("Rehab")
+my_function("Kalthom")
+my_function("Mohammad")
