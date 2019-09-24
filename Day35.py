b4adae91cc02e629ae28c57eb214892804295c58
Kalthom Musa <kalthommusa12345@gmail.com>
2019-09-24 19:28:25 +0300

    Day35

---
+++ Day35.py
@@ -0,0 +1,11 @@
+"""
+a lambda function that adds 10 
+to the number passed in as an argument 
+and print the result
+"""
+x=lambda a:a + 10
+print(x(5))
+
+#a lambda function that takes multiple arguments
+x=lambda a,b : a * b
+print(x(5,7))
