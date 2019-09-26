836adfef09830564aed6aa72c6f9a3b282584ea9
Kalthom Musa <kalthommusa12345@gmail.com>
2019-09-26 19:43:25 +0300

    Day37

---
+++ Day37.py
@@ -0,0 +1,14 @@
+#create an array containing car names
+cars=["Ford", "Volvo", "BMW"]
+print(cars)
+
+#get the value of the first array item
+print(cars[0])
+
+#modify the value of the first array item
+cars[0]="Toyota"
+print(cars[0])
+print(cars)
+
+#return the number of elements in the cars array
+print(len(cars))
