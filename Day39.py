6e9075af1605015effb056efdffe45aa95e46659
Kalthom Musa <kalthommusa12345@gmail.com>
2019-09-28 17:45:31 +0300

    Day39

---
+++ Day39.py
@@ -0,0 +1,12 @@
+#a recursive function that calculates the power of a number
+def power(base,exp): 
+    if(exp==1):
+        return(base)
+
+    if(exp!=1):
+        return (base*power(base,exp-1))
+"""
+calling the recursive function
+and passing the values of the base & exponential 
+"""
+print("Result:",power(5,3))
