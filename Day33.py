c47358e8656384b9cc91cdce5d22f6321b95949c
Kalthom Musa <kalthommusa12345@gmail.com>
2019-10-05 20:42:41 +0300

    Day33

--- Day33.py
+++ Day33.py
@@ -1,5 +1,5 @@
 #task2 for week5
 #nested loop,prints each odd number along with all even numbers 
-for i in range(1,18,2):
-	for j in range(0,18,2):
-		print(i,j)
+for odd in range(3,18,2):
+	for even in range(2,17,2):
+		print(odd,even)
