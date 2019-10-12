c0b3cbaad6af02a7d440b975bea7d48cc4953886
Kalthom Musa <kalthommusa12345@gmail.com>
2019-10-12 22:58:38 +0300

    Day53

--- Day53.py
+++ Day53.py
@@ -1,14 +1,14 @@
 #task 1 for week8
-import task1_module
-#call the addition function from the task1_module module and pass the parameters 
-add=task1_module.addition(8,1)
+import Day53_module
+#call the addition function from the Day53_module module and pass the parameters 
+add=Day53_module.addition(8,1)
 print(add)
-#call the subtraction function from the task1_module module and pass the parameters 
-sub=task1_module.subtraction(4,2)
+#call the subtraction function from the Day53_module module and pass the parameters 
+sub=Day53_module.subtraction(4,2)
 print(sub)
-#call the multiplication function from the task1_module module and pass the parameters 
-multi=task1_module.multiplication(6,6)
+#call the multiplication function from the Day53_module module and pass the parameters 
+multi=Day53_module.multiplication(6,6)
 print(multi)
-#call the division function from the task1_module module and pass the parameters 
-div=task1_module.division(8,2)
+#call the division function from the Day53_module module and pass the parameters 
+div=Day53_module.division(8,2)
 print(div)
