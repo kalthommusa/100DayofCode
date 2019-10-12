65ec87ddba153db56a317b1b2ed131660efa4bf5
Kalthom Musa <kalthommusa12345@gmail.com>
2019-10-12 23:08:43 +0300

    Day53_module

---
+++ Day53_module.py
@@ -0,0 +1,16 @@
+#create a module for task1
+def addition(x,y):
+	add=x+y
+	return add
+
+def subtraction(x,y):
+	sub=x-y
+	return sub
+
+def multiplication(x,y):
+	mult=x*y
+	return mult
+
+def division(x,y):
+	div=x/y
+	return div
