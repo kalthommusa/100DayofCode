456990e9279b4c682fbce2791baddacd5b45487b
Kalthom Musa <kalthommusa12345@gmail.com>
2019-10-22 00:04:51 +0300

    Day63

---
+++ Day63.py
@@ -0,0 +1,7 @@
+"""
+ask for the user's name, and when the user entered the name, 
+the name gets printed to the screen
+"""
+print("Hi Geek, What's your name?")
+day63=input()
+print("Great!..keep learning",day63)
