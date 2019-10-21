c684e1fdac8f7c21543d69d92ae53250ff1e6098
Kalthom Musa <kalthommusa12345@gmail.com>
2019-10-22 00:21:45 +0300

    Day62

---
+++ Day62.py
@@ -0,0 +1,6 @@
+#Import and use "camelcase"
+import camelcase
+c=camelcase.CamelCase()
+day62="successfuly passed day62"
+#this method capitalizes the first letter of each word
+print(c.hump(day62))
