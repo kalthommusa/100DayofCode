7882237ed812f258224b523b70822e28c8608ce4
Kalthom Musa <kalthommusa12345@gmail.com>
2019-10-23 23:39:01 +0300

    Day65

---
+++ Day65.py
@@ -0,0 +1,5 @@
+#Add a placeholder where you want to display the number that the user will enter
+print("Hi Geek, which day is this?")
+day65=input()
+x="You have Successfully Passed Day{}...Great Work!"
+print(x.format(day65))
