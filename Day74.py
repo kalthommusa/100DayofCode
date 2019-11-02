c569c74b1888471191024915e5a82900a6d56d64
Kalthom Musa <kalthommusa12345@gmail.com>
2019-11-02 14:33:52 +0300

    Day74

---
+++ Day74.py
@@ -0,0 +1,12 @@
+#open the file "Day74_file.txt" and read it
+day74=open("Day74_file.txt","rt")
+print(day74.read())
+day74.close()
+print(_______________________________)
+#Open the file and append content to the file
+day74=open("Day74_file.txt","a")	
+day74.write("The best way we learn anything is by practice and exercise questions")
+day74.close()
+#open the file and read it
+day74=open("Day74_file.txt","rt")
+print(day74.read())
